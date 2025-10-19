2729a322f3d9dc13116875d5ec1e3de5c7f46f11
duy260414-lang <duy260414@gmail.com>
2025-10-19 15:41:47 +0700

    Add files via upload

--- Scriptdemo (4).lua
+++ Scriptdemo (4).lua
@@ -31,7 +31,7 @@ loadstring(game:HttpGet(("https://raw.githubusercontent.com/daucobonhi/Ui-Redz-V
       
 ------ Tab
      local Tab1o = MakeTab({Name = "Script Farm"})
-     local Tab1o = MakeTab({Name = "script auto hop"})
+     local Tab2o = MakeTab({Name = "script auto hop"})
 ------- BUTTON
     
     AddButton(Tab1o, {
